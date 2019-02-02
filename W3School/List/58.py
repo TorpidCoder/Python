6ab445a45535412a7e7bb4e14097d2694baeba48
Sahil Nagpal <sahilexemplary@gmail.com>
2019-02-02 12:04:04 +0530

    more list

---
+++ W3School/List/58.py
@@ -0,0 +1,8 @@
+Sample_data = [1, 3, 5, 7, 9, 10],[2, 4, 6, 8]
+
+arr = []
+
+for vals in Sample_data:
+    arr+=vals
+
+print(arr)
---
+++ W3School/List/63.py
@@ -0,0 +1,4 @@
+list = [1,2,3,4,5]
+string = "steps"
+
+print( ['emp{0}'.format(i) for i in list])
