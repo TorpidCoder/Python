8291f87ace785c3f417f9a2f36ae55b1f334e8da
sahilthenagpal <thisissahilnagpal@gmail.com>
2020-07-21 22:07:54 -0400

    test code

---
+++ PythonCourse/LeetCode/Easy/lowerCase.py
@@ -0,0 +1,11 @@
+__author__ = "ResearchInMotion"
+
+#Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+#
+# Example 1:
+#
+# Input: "Hello"
+# Output: "hello"
+
+inputstring = "Hello"
+print(inputstring.lower())
